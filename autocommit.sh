9d6e4bef6421a6e0f57cc3a5963aa16e68103439
BrendanM720 <brendan.meteer@gmail.com>
2024-09-18 08:35:02 -0400

    Autocommit from 2024-09-18 08:35:02
    Klipper version: v0.12.0-118-g18de421c4
    
    Moonraker version: v0.8.0-320-g6b1b8c5
    
    Mainsail version: v2.10.0

--- autocommit.sh
+++ autocommit.sh
@@ -34,7 +34,8 @@ mainsail_folder=~/mainsail
 ### By default that is 'main'
 branch=main
 
-db_file=~/printer_data/database/moonraker-sql.db
+#db_file=~/printer_data/database/moonraker-sql.db
+db_file=~/printer_data/database/backup.txt
 
 #####################################################################
 #####################################################################
